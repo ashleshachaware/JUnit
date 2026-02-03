a4e44acee0031b1084f6b5d2d67e7924da7b1b01
ashleshachaware <ashlesha.chaware@cumminscollege.in>
2026-02-03 14:41:51 +0530

    added simple addition code

---
+++ add.java
@@ -0,0 +1,9 @@
+class add{ 
+    public int addNumbers(int a, int b) {
+        int sum = 0;
+        sum = a + b;
+        System.out.println("Sum is: " + sum);
+        return sum;
+    }
+}
+    
