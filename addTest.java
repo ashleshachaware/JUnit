5db40fea38ecdc5e7d9956c686e3da096b8c7d85
ashleshachaware <ashlesha.chaware@cumminscollege.in>
2026-02-03 14:52:52 +0530

    added tests and ran tests

---
+++ addTest.java
@@ -0,0 +1,11 @@
+import java.beans.Transient;
+import org.junit.*;
+public class addTest {
+    @Test
+    public void addTest1(){
+        add add_var = new add();
+        Assert.assertEquals(7, add_var.addNumbers(4, 3));
+        Assert.assertEquals(11, add_var.addNumbers(5, 6));
+        Assert.assertEquals(-6, add_var.addNumbers(-4, -2));
+    }
+}
